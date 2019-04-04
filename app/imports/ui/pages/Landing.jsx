ddc90896a6a20b257308d69aabc7a659cc3483c7
Yusuke Hatanaka <yusukemh@hawaii.edu>
2019-04-04 12:24:26 -1000

    Initial commit

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const headerStyle = { color: "white", fontSize: "4rem" };
+    const divStyle = { backgroundColor: "#006400" };
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='myuhclub-landing-background'>
+          <Grid verticalAlign='middle' textAlign='center' columns={2}>
+            <Grid.Column>
+              <p>welcome</p>
+            </Grid.Column>
+            <div style={divStyle}>
+              <Grid.Column>
+                <Grid.Row>
+                  <p style={headerStyle}>MyUH Club</p>
+                </Grid.Row>
+                <Grid.Column>
+                  <Image size='small' circular src="/images/meteor-logo.png"/>
+                </Grid.Column>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+                <Grid.Column>
+                  <h1>Welcome to this template</h1>
+                  <p>Now get to work and modify this app!</p>
+                </Grid.Column>
+              </Grid.Column>
+            </div>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+          </Grid>
+        </div>
 
-        </Grid>
     );
   }
 }
