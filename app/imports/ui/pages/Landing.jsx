d35a9fea4862d5603bf46d809dca3e8514e5015b
Yusuke Hatanaka <yusukemh@hawaii.edu>
2019-04-09 11:24:58 -1000

    minor cleanup

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,16 +4,14 @@ import { Grid, Icon, Header, Button, List } from 'semantic-ui-react';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
-    const padding = { padding: "2rem" };
-    const opacity = { opacity: "1" };
     return (
-        <div className='myuhclub-landing-background' style={padding}>
+        <div className='myuhclub-landing-background'>
           <Grid verticalAlign='middle' textAlign='center' columns={2}>
             <Grid.Column>
               <Header size='huge' inverted>MyUH Club</Header>
             </Grid.Column>
 
-            <Grid.Column className='myuhclub-landing-background-right' style={opacity}>
+            <Grid.Column className='myuhclub-landing-background-right'>
               <Grid.Row>
                 <Header as='h3' inverted><Icon name='search' size='huge' inverted/>Search for Your Club</Header>
                 <Header as='h3' inverted><Icon name='star' size='huge' inverted/>Add to Your Favorite</Header>
