1f84161c89e8aab28874d1350df4dfb03cac7d0a
Yusuke Hatanaka <yusukemh@hawaii.edu>
2019-04-09 10:51:31 -1000

    complete
    first draft completed for the landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import { Grid, Image, Icon, Header, Button, Container } from 'semantic-ui-react';
+import { Grid, Image, Icon, Header, Button, Container, List } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
-    // const headerStyle = { color: "white", fontSize: "4rem" };
-    const divStyle = { backgroundColor: "#006400" };
+    const divStyle = { backgroundColor: "#006400", opacity: "0.8", borderRadius: "15px" };
+    const temp = {padding: "2rem"};
     return (
-        <div className='myuhclub-landing-background'>
+        <div className='myuhclub-landing-background' style={temp}>
           <Grid verticalAlign='middle' textAlign='center' columns={2}>
-            <Grid.Column className='myuhclub-landing-background'>
-              <Header as='h3' inverted><Icon name='search' size='huge' inverted/>Search for Your Club</Header>
-              <Header as='h3' inverted><Icon name='star' size='huge' inverted/>Add to Your Favorite</Header>
-              <Header as='h3' inverted><Icon name='users' size='huge' inverted/>Expand Your Community</Header>
+            <Grid.Column>
+              <Header size='huge' inverted>MyUH Club</Header>
             </Grid.Column>
 
             <Grid.Column style={divStyle}>
-              <Header size='huge' inverted>MyUH Club</Header>
 
               <Grid.Row>
                 <Header as='h3' inverted><Icon name='search' size='huge' inverted/>Search for Your Club</Header>
@@ -24,28 +21,22 @@ class Landing extends React.Component {
                 <Header as='h3' inverted><Icon name='users' size='huge' inverted/>Expand Your Community</Header>
               </Grid.Row>
 
-              <Grid.Row>
-                <Button.Group>
-                  <Button color='teal' basic>Sign In</Button>
-                  <Button.Or/>
-                  <Button positive basic>Sign Up</Button>
-                </Button.Group>
-
-              </Grid.Row>
-
-              <Grid.Row>
-                <Button basic color='black' size='huge'>
-                  <Icon name='search'/>
-                  Search
-                </Button>
-              </Grid.Row>
-
-              <Grid.Column>
-                <h1>Welcome to this template</h1>
-                <p>Now get to work and modify this app!</p>
-              </Grid.Column>
+              <List>
+                <List.Item>
+                  <Button.Group>
+                    <Button color='teal' basic>Sign In</Button>
+                    <Button.Or/>
+                    <Button positive basic>Sign Up</Button>
+                  </Button.Group>
+                </List.Item>
+                <List.Item>
+                  <Button basic color='black' size='huge'>
+                    <Icon name='search'/>
+                    Search
+                  </Button>
+                </List.Item>
+              </List>
             </Grid.Column>
-
           </Grid>
         </div>
 
