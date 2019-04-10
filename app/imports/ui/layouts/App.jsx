ea2e44e89d04e83b4e5655bf43f1d1c97a3179ad
Yusuke Hatanaka <yusukemh@hawaii.edu>
2019-04-09 20:00:59 -1000

    changed the path

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -25,7 +25,7 @@ class App extends React.Component {
           <div>
             <NavBar/>
             <Switch>
-              <Route exact path="/" component={ReportProblem}/>
+              <Route exact path="/" component={Landing}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
               <ProtectedRoute path="/list" component={ListStuff}/>
