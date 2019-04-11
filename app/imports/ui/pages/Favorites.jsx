d00168cc7f0f27963e7ed34a78b5fb6598adbbe4
ronnie-kauanoe <rkauanoe@hawaii.edu>
2019-04-11 08:35:28 -1000

    Commented out Favorites subscription

--- app/imports/ui/pages/Favorites.jsx
+++ app/imports/ui/pages/Favorites.jsx
@@ -4,7 +4,6 @@ import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Clubs } from '/imports/api/club/club';
 import Club from '/imports/ui/components/Club';
-import { Favorites } from '/imports/api/favorite/favorite';
 
 /** A simple static component to render some text for the search page. */
 class Favorites extends React.Component {
@@ -33,10 +32,9 @@ Favorites.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Clubs');
-  const subscription2 = Meteor.subscribe('Favorites');
+  //const subscription = Meteor.subscribe('Clubs');
   return {
-    favorites: Favorites.find({}).fetch(),
-    ready: subscription.ready() && subscription2.ready(),
+    //favorites: Favorites.find({}).fetch(),
+    ready: subscription.ready(),
   };
 })(Favorites);
