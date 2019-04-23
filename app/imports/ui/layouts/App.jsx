b174459e7324f6d9cfc6a15eae7d1c27046c5951
ronnie-kauanoe <rkauanoe@hawaii.edu>
2019-04-23 09:10:56 -1000

    Renamed Favorites, Changed App

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -27,7 +27,7 @@ import Signout from '../pages/Signout';
 import ReportProblem from '../pages/ReportProblem';
 import LogIn from '../pages/LogIn';
 import Search from '../pages/Search';
-import Favorites from '../pages/Favorites';
+import FavoritesPage from '../pages/FavoritesPage';
 
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
@@ -41,7 +41,7 @@ class App extends React.Component {
               <Route exact path="/" component={Landing}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
-              <Route path='/favorites' component={Favorites}/>
+              <Route path='/favorites' component={FavoritesPage}/>
               <Route path='/search' component={Search}/>
               <ProtectedRoute path="/list" component={AboutUsList}/>
               <ProtectedRoute path="/add" component={StudentHomepage}/>
