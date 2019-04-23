14ba0c6f9f53f42227bd6075c113e21dc44f6312
Yusuke Hatanaka <yusukemh@hawaii.edu>
2019-04-22 17:28:21 -1000

    Update MyUhLanding.jsx

--- app/imports/ui/pages/MyUhLanding.jsx
+++ app/imports/ui/pages/MyUhLanding.jsx
@@ -9,7 +9,7 @@ class Landing extends React.Component {
     return (
         <Container>
           <Grid>
-            <Grid.Row columns={2}>
+            <Grid.Row columns={2} verticalAlign='middle'>
               <Grid.Column width={6}>
                 <Image src="/images/UH-Manoa-min.png"/>
               </Grid.Column>
@@ -19,7 +19,7 @@ class Landing extends React.Component {
                   Registered Independent Organizations
                 </Header>
                 <Header as='h3'>
-                  University of Hawaii has more than 200 Registered Independent Organizations.
+                  University of Hawaii has more than 200 Registered Independent Organizations.<br/>
                   When it comes to finding one for you, however,
                   it is a hard task to go through every organizations!
                   Here, MyUH Club can help you
